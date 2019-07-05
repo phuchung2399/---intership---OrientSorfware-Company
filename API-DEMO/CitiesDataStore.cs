5de0bfb6200cec3dd1bc11d82e06634c86f8c5be
phuchung2399 <phuchung2399@gmail>
2019-07-05 12:04:12 +0700

    code api by asp.net core

---
+++ API-DEMO/CitiesDataStore.cs
@@ -0,0 +1,83 @@
+﻿using APIDEMO.Controllers;
+using APIDEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDEMO
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    id = 1,
+                    name = "menu",
+                    url = "/",
+                    applySubMenus = new List<subMenu>()
+                    {
+                        new subMenu()
+                        {
+                              id = 1,
+                             name = "menu",
+                             url = "/"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    id = 2,
+                    name = "menu2",
+                    url = "/",
+                     applySubMenus = new List<subMenu>()
+                    {
+                        new subMenu()
+                        {
+                              id = 2,
+                             name = "menu3",
+                             url = "/"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    id = 3,
+                    name = "menu4",
+                    url = "/",
+                     applySubMenus = new List<subMenu>()
+                    {
+                        new subMenu()
+                        {
+                              id = 3,
+                             name = "menu",
+                             url = "/"
+                        }
+                    }
+                },
+                  new CityDto()
+                {
+                    id = 4,
+                    name = "menu",
+                    url = "/",
+                     applySubMenus = new List<subMenu>()
+                    {
+                        new subMenu()
+                        {
+                              id = 4,
+                             name = "menu5",
+                             url = "/"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ API-DEMO/Models/subMenuforsub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDEMO.Models
+{
+    public class subMenuforsub
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+}
